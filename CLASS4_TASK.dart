a78c7c44bac07b0d9065d70b1e263838ece1cc34
jameelkhoso4@gmail.com <jameelkhoso4@gmail.com>
2023-07-26 22:57:36 +0500

    Class Task

---
+++ CLASS4_TASK.dart
@@ -0,0 +1,23 @@
+void main(){
+  int num1=99;
+
+if(num1%5==0  || num1%7==0){
+
+   if(num1%5==0  && num1%7==0){
+print("num devisible by both");
+   }
+   else if(num1%5==0){
+      print("number dvisible by only 5");
+   }else{
+    print("number only divisible by 7");
+   }
+
+
+
+}else{
+print("number cant be divisible by both");
+}
+
+
+
+}
