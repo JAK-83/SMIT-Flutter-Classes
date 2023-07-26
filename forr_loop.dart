d849ff65a88cbd850a42c1c135fe0b6d7dea7c6b
jameelkhoso4@gmail.com <jameelkhoso4@gmail.com>
2023-07-26 23:00:18 +0500

    Loop Task

---
+++ forr_loop.dart
@@ -0,0 +1,22 @@
+//import 'dart:html';
+
+ void main(){
+List a=[32,3,32,32,3,33,453,5,53,43];
+var b=0;
+print(a);
+
+for(var c in a){
+print(c);
+}
+
+for (b=0;b<a.length;b++){
+  print(b);
+}
+
+
+
+
+
+}
+
+
