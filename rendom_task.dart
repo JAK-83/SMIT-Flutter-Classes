d9f19ffc8bd42400f7b05f28ca1d016f3e5ec38c
jameelkhoso4@gmail.com <jameelkhoso4@gmail.com>
2023-07-26 23:06:31 +0500

    Task

---
+++ rendom_task.dart
@@ -0,0 +1,20 @@
+void main(){
+
+List haha=[
+     {
+      0:0,
+      1:[1]
+     },
+      {"2":"2"},
+       { 1:{
+          "hehe":{"hehe":"bye"}
+        }
+      }
+
+];
+
+
+print(haha[2][1]["hehe"]["hehe"]);
+print(haha[0][0]);
+
+}
